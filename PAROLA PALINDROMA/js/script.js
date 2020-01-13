9edc601eabf729ff2bc661988b64146bd3ecdc65
Marco <marco.carone@libero.it>
2020-01-13 17:04:50 +0100

    altro metodo per es palindroma

--- PAROLA PALINDROMA/js/script.js
+++ PAROLA PALINDROMA/js/script.js
@@ -1,24 +1,50 @@
 // Scrivi una funzione per capire se la parola è palindroma
-var parolaUtente = prompt("inserisci una parola");
 
-if (parolaUtente == invertiStringa(parolaUtente)) {
+var parolaUtente = prompt("inserisci una parola");
+var parola = [ ];
+var parolaInversa = [ ];
+for ( var i = 0; i < parolaUtente.length; i++) {
+  parola.push(parolaUtente[i]);
+  console.log(parola);
+}
+
+for ( var i = parolaUtente.length - 1; i >= 0; i--) {
+  parolaInversa.push(parolaUtente[i]);
+ console.log(parolaInversa);
+}
+ for (var i = 0; i < parolaUtente.length; i++) {
+if (parola[i] == parolaInversa[i]) {
   document.getElementById("azione").innerHTML = parolaUtente + " è una parola palindroma";
 } else {
   document.getElementById("azione").innerHTML = parolaUtente + " Non è una parola palindroma";
 }
+   }
 
 
-function invertiStringa(stringa) {
 
-  var splitStringa = stringa.split("");
-  // Questo genera una array con i caratteri della parola
+// RISOLUZIONE ESERCIZIO CON SPLIT E JOIN
 
-  var invertiArray = splitStringa.reverse();
-  // questo inverte gli indici dell'array
-
-  // Step 3. Use the join() method to join all elements of the array into a string
-  var unisciArray = invertiArray.join("");
-  // unisce gli indici in una stringa
-
-  return unisciArray;
-}
+// Scrivi una funzione per capire se la parola è palindroma
+// var parolaUtente = prompt("inserisci una parola");
+//
+// if (parolaUtente == invertiStringa(parolaUtente)) {
+//   document.getElementById("azione").innerHTML = parolaUtente + " è una parola palindroma";
+// } else {
+//   document.getElementById("azione").innerHTML = parolaUtente + " Non è una parola palindroma";
+// }
+//
+//
+// function invertiStringa(stringa) {
+//
+//   var splitStringa = stringa.split("");
+//   // Questo genera una array con i caratteri della parola
+//
+//   var invertiArray = splitStringa.reverse();
+//   // questo inverte gli indici dell'array
+//
+//   // Step 3. Use the join() method to join all elements of the array into a string
+//   var unisciArray = invertiArray.join("");
+//   // unisce gli indici in una stringa
+//
+//   return unisciArray;
+// }
