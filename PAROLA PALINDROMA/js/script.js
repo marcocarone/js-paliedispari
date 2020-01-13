e95a16416e479d2f59cd3419cfcf3d34d7fe1697
Marco <marco.carone@libero.it>
2020-01-13 17:17:54 +0100

    creata funzione

--- PAROLA PALINDROMA/js/script.js
+++ PAROLA PALINDROMA/js/script.js
@@ -1,24 +1,34 @@
 // Scrivi una funzione per capire se la parola è palindroma
 
 var parolaUtente = prompt("inserisci una parola");
-var parola = [ ];
-var parolaInversa = [ ];
-for ( var i = 0; i < parolaUtente.length; i++) {
-  parola.push(parolaUtente[i]);
-  console.log(parola);
+var parola = [];
+var parolaInversa = [];
+
+function palindroma() {
+  for (var i = 0; i < parolaUtente.length; i++) {
+    parola.push(parolaUtente[i]);
+    console.log(parola);
+  }
+
+  for (var i = parolaUtente.length - 1; i >= 0; i--) {
+    parolaInversa.push(parolaUtente[i]);
+    console.log(parolaInversa);
+  }
+  for (var i = 0; i < parolaUtente.length; i++) {
+    if (parola[i] == parolaInversa[i]) {
+      return true;
+    } else {
+      return false
+    }
+  }
+
 }
 
-for ( var i = parolaUtente.length - 1; i >= 0; i--) {
-  parolaInversa.push(parolaUtente[i]);
- console.log(parolaInversa);
-}
- for (var i = 0; i < parolaUtente.length; i++) {
-if (parola[i] == parolaInversa[i]) {
+if (palindroma(parolaUtente) == true) {
   document.getElementById("azione").innerHTML = parolaUtente + " è una parola palindroma";
 } else {
   document.getElementById("azione").innerHTML = parolaUtente + " Non è una parola palindroma";
 }
-   }
 
 
 
